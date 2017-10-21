85c42f9a26d4944dc746111738679ce5a4dc73eb
Timo Wang <chiuanw@yahoo-inc.com>
2017-10-21 18:01:41 +0800

    added lazy loading

--- schema.js
+++ schema.js
@@ -28,11 +28,13 @@ const BookType = new GraphQLObjectType({
     fields: () => ({
         title: {
             type: GraphQLString,
-            resolve: xml => xml.title[0]
+            // resolve: xml => xml.title[0]
+            resolve: xml => xml.GoodreadsResponse.book[0].title[0]
         },
         isbn: {
             type: GraphQLString,
-            resolve: xml => xml.isbn[0]
+            // resolve: xml => xml.isbn[0]
+            resolve: xml => xml.GoodreadsResponse.book[0].isbn[0]
         }
     })
 })
@@ -48,7 +50,15 @@ const AuthorType = new GraphQLObjectType({
         },
         books: {
             type: new GraphQLList(BookType),
-            resolve: xml => xml.GoodreadsResponse.author[0].books[0].book
+            resolve: xml => {
+                const ids = xml.GoodreadsResponse.author[0].books[0].book.map(elem => elem.id[0]._);
+                return Promise.all(ids.map(id =>
+                    fetch(`https://www.goodreads.com/book/show/${id}.xml?key=HtwnenHlS6Bokx4qeKm4gA`)
+                        .then(response => response.text())
+                        .then(parseXML)
+                ));
+                // xml.GoodreadsResponse.author[0].books[0].book
+            }
         }
     })
 });
